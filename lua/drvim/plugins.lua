2dc65df706e441b870b4e2a87a26c53f22c2acc0
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-18 16:34:29 +0200

    documentation: about 'use' function in plugins.lua

--- lua/drvim/plugins.lua
+++ lua/drvim/plugins.lua
@@ -1,5 +1,16 @@
--- note that the 'use' function automatically loads
--- a configuration from the configs folder if available
+--
+-- Note that the 'use' function here is not the original packer
+-- 'use' function. It is a function that automatically loads the
+-- plugin and its configuration from the 'configs' folder. For
+-- instance for package 'foo', a configuration file is expected
+-- in 'drvim/configs/foo.lua'.
+--
+-- Some packages have a '.' in their name (e.g. 'bufferline.nvim'),
+-- in which case the expected configuration file should have the
+-- same name as the package name except that '.' are replaced by
+-- '-', this to avoid any lua import problems.
+--
+
 return function(use)
     use "wbthomason/packer.nvim"
     use "projekt0n/github-nvim-theme"
