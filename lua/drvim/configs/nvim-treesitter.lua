e10f1376b8a25b56d97eaf2b480cb0fee2ec2430
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-18 15:41:12 +0200

    feature: treesitter for improved syntax highlighting
    For now only c, lua and python are enabled. More to follow...

---
+++ lua/drvim/configs/nvim-treesitter.lua
@@ -0,0 +1,26 @@
+require("nvim-treesitter.configs").setup {
+    ensure_installed = { "c", "lua", "python" },
+    sync_install = true,
+    ignore_install = {},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+    },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+    },
+    indent = {
+        enable = true,
+        disable = { "yaml" }
+    },
+    rainbow = {
+        enable = true,
+        disable = { "html" },
+        extended_mode = false,
+        max_file_lines = nil,
+    },
+    autopairs = { enable = true },
+    autotag = { enable = true },
+    incremental_selection = { enable = true },
+}
--- lua/drvim/plugins.lua
+++ lua/drvim/plugins.lua
@@ -4,9 +4,17 @@ if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
     vim.cmd "packadd packer.nvim"
 end
 
+local drvim_config = function(package_name)
+    require("drvim.configs." .. package_name)
+end
+
 local function startup(use)
     use {
         "wbthomason/packer.nvim",
+    }
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        config = drvim_config
     }
 end
 
