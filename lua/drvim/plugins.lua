4403e5a1b3ca568503fd9476741f4d1f47ca1a9f
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-19 19:17:04 +0200

    fix: 'nvim-ts-context-commentstring' was not installed properly

--- lua/drvim/plugins.lua
+++ lua/drvim/plugins.lua
@@ -18,28 +18,29 @@
 --      e.g. 'drvim/configs/bufferline-nvim.lua'
 return function(use, use_with_config)
 
-    use "wbthomason/packer.nvim"                            -- manage packer itself when updating
-    use "kheaactua/aosp-vim-syntax"                         -- syntax highlight for Android.bp, XML manifest, AIDL, HIDL, etc.
-    use "stevearc/dressing.nvim"                            -- improved vim.input() and vim.select() used e.g. for LSP renaming
-    use "nvim-lua/popup.nvim"                               -- an implementation of the Popup API from vim in Neovim
-    use "nvim-lua/plenary.nvim"                             -- useful lua functions used by lots of plugins
-    use "farmergreg/vim-lastplace"                          -- rememnber position in file when restarting
-    use "tpope/vim-fugitive"                                -- magit style plugin for VIM
-    use "fedepujol/move.nvim"                               -- move lines around in V mode
-    use "MunifTanjim/nui.nvim"                              -- ui framework used by neo-tree
-    use "valloric/listtoggle"                               -- toggle quick and location lists
-    use "moll/vim-bbye"                                     -- allow to kill buffer without exiting VIM
+    use "wbthomason/packer.nvim"                                    -- manage packer itself when updating
+    use "kheaactua/aosp-vim-syntax"                                 -- syntax highlight for Android.bp, XML manifest, AIDL, HIDL, etc.
+    use "stevearc/dressing.nvim"                                    -- improved vim.input() and vim.select() used e.g. for LSP renaming
+    use "nvim-lua/popup.nvim"                                       -- an implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim"                                     -- useful lua functions used by lots of plugins
+    use "farmergreg/vim-lastplace"                                  -- rememnber position in file when restarting
+    use "tpope/vim-fugitive"                                        -- magit style plugin for VIM
+    use "fedepujol/move.nvim"                                       -- move lines around in V mode
+    use "MunifTanjim/nui.nvim"                                      -- ui framework used by neo-tree
+    use "valloric/listtoggle"                                       -- toggle quick and location lists
+    use "moll/vim-bbye"                                             -- allow to kill buffer without exiting VIM
+    use "JoosepAlviste/nvim-ts-context-commentstring"               -- select comment string based on TS parser
 
-    use_with_config "projekt0n/github-nvim-theme"           -- github colorscheme
-    use_with_config "kyazdani42/nvim-web-devicons"          -- icons for bufferline and others.
-    use_with_config "nvim-treesitter/nvim-treesitter"       -- improved syntax hightlighing
-    use_with_config "nvim-ts-context-commentstring"         -- select comment string based on TS parser
-    use_with_config "terrortylor/nvim-comment"              -- toggle comment blocks/lines
-    use_with_config "lukas-reineke/indent-blankline.nvim"   -- show indent guides and hidden tabs/space/return
-    use_with_config "ntpeters/vim-better-whitespace"        -- hightlight and strip trailing whitespaces
-    use_with_config "akinsho/toggleterm.nvim"               -- improved persistent embedded terminal
-    use_with_config "lewis6991/gitsigns.nvim"               -- display green/red/blue guidelines for git
-    use_with_config "windwp/nvim-autopairs"                 -- automativally insert matching pair for parens and quotes
+    use_with_config "projekt0n/github-nvim-theme"                   -- github colorscheme
+    use_with_config "kyazdani42/nvim-web-devicons"                  -- icons for bufferline and others.
+    use_with_config "nvim-treesitter/nvim-treesitter"               -- improved syntax hightlighing
+    use_with_config "terrortylor/nvim-comment"                      -- toggle comment blocks/lines
+    use_with_config "lukas-reineke/indent-blankline.nvim"           -- show indent guides and hidden tabs/space/return
+    use_with_config "ntpeters/vim-better-whitespace"                -- hightlight and strip trailing whitespaces
+    use_with_config "akinsho/toggleterm.nvim"                       -- improved persistent embedded terminal
+    use_with_config "lewis6991/gitsigns.nvim"                       -- display green/red/blue guidelines for git
+    use_with_config "windwp/nvim-autopairs"                         -- automativally insert matching pair for parens and quotes
+
 
     -- tabs line but for buffers
     use_with_config {
