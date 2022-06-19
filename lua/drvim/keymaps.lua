8246abd5199949bf9b79f33e2485f7f0c0e53e49
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-19 22:38:54 +0200

    feature: improved keymap

--- lua/drvim/keymaps.lua
+++ lua/drvim/keymaps.lua
@@ -1,20 +1,43 @@
-local function map(mode, key, command)
+local function noremap(mode, key, command)
     local options = {noremap=true, silent=true}
-    return vim.api.nvim_set_keymap(mode, key, command, options)
+    vim.api.nvim_set_keymap(mode, key, command, options)
 end
 
-local function map_all_modes(key, command)
-    map("n", key, command)
-    map("i", key, "<C-\\><C-N>" .. command)
-    map("v", key, "<C-\\><C-N>" .. command)
-end
+-- Ctrl+P to open file using fuzzy finder
+noremap("n", "<C-p>", "<CMD>ProjectFiles<CR>")
+noremap("n", "<C-p>", "<CMD>ProjectFiles<CR>")
+noremap("i", "<C-p>", "<C-\\><C-N><CMD>ProjectFiles<CR>")
 
--- ctrl+p to open file
-map_all_modes("<C-p>", "<CMD>ProjectFiles<CR>")
+-- Ctrl+/ to toggle comment
+noremap("n", "<C-_>", "<CMD>CommentToggle<CR>j")
+noremap("i", "<C-_>", "<C-\\><C-N><CMD>CommentToggle<CR>ji")
+noremap("v", "<C-_>", ":'<,'>CommentToggle<CR>gv<esc>j")
+
+-- Tab/Shift+tab to indent/dedent
+noremap("v", "<Tab>", ">gv")
+noremap("n", "<Tab>", "v><C-\\><C-N>")
+noremap("v", "<S-Tab>", "<gv")
+noremap("n", "<S-Tab>", "v<<C-\\><C-N>")
+noremap("i", "<S-Tab>", "<C-\\><C-N>v<<C-\\><C-N>^i")
+
+-- Alt+Up/Down to move lines around
+noremap("n", "<A-Down>", ":MoveLine(1)<CR>")
+noremap("n", "<A-Up>", ":MoveLine(-1)<CR>")
+noremap("v", "<A-Down>", ":MoveBlock(1)<CR>")
+noremap("v", "<A-Up>", ":MoveBlock(-1)<CR>")
+noremap("i", "<A-Down>", "<C-\\><C-N>:MoveLine(1)<CR>i")
+noremap("i", "<A-Up>", "<C-\\><C-N>:MoveLine(-1)<CR>i")
+
+-- Ctrl+Left/Right use word boundary
+noremap("n", "<C-Left>", "b")
+noremap("n", "<C-Right>", "w")
+
+-- Alt+Left/Right to navigate jumplist
+noremap("n", "<A-Left>", "<C-O>")
+noremap("n", "<A-Right>", "<C-I>")
 
 
--- ctrl+shift+/ to toggle comment
-map("n", "<C-_>", "<CMD>CommentToggle<CR>j")
-map("i", "<C-_>", "<C-\\><C-N><CMD>CommentToggle<CR>ji")
-map("v", "<C-_>", ":'<,'>CommentToggle<CR>gv<esc>j")
+
+
+
 
