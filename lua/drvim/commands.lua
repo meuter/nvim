a2ed0323d8682af1f5b6517aee974fa71eafb6e9
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-19 23:45:38 +0200

    feature: renamed command to CloseOthers

--- lua/drvim/commands.lua
+++ lua/drvim/commands.lua
@@ -2,7 +2,7 @@ local def = vim.api.nvim_create_user_command
 
 def("Close", "Bd", { desc = "Close Current Buffer" })
 def("CloseAll", ":bufdo Bd", { desc = "Close All Buffers"})
-def("CloseAllExceptCurrent", "%bd|e#|bd#", { desc="Close All Buffers Except Current"})
+def("CloseOthers", "%bd|e#|bd#", { desc="Close All Buffers Except Current"})
 def("Format", vim.lsp.buf.formatting, { desc = "Format Current Buffer Using LSP"})
 def("InstallLanguages", require("drvim.languages").install, { desc="Install Languages (TS Parser and LSP Servers)"})
 def("ProjectFiles", require("drvim.utils").project_files, { desc="Fuzzy File Search Through Project Files" })
