d1875e8476c660141fd738f5337bc73e4a3003cb
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-19 19:31:12 +0200

    feature: provide border window for the plume documentation

--- lua/drvim/configs/nvim-cmp.lua
+++ lua/drvim/configs/nvim-cmp.lua
@@ -35,6 +35,11 @@ cmp.setup {
             luasnip.lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
+    window = {
+        documentation = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        },
+    },
     mapping = {
         ["<Up>"] = cmp.mapping.select_prev_item(),
         ["<Down>"] = cmp.mapping.select_next_item(),
