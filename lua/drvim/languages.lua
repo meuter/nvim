bb7c7b87bb004a7f6f7563b5106d7885d62146ab
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-20 14:38:56 +0200

    feature: added support for bash script using LSP

--- lua/drvim/languages.lua
+++ lua/drvim/languages.lua
@@ -3,8 +3,8 @@ return {
     ["python"]      = "pyright",
     ["lua"]         = "sumneko_lua",
     ["dockerfile"]  = "dockerls",
+    ["bash"]        = "bashls",
     -- TODO(cme): support html, js, ts, react
-    -- TODO(cme): support bash
     -- TODO(cme): support rust
     -- TODO(cme): support go
 }
---
+++ samples/bash/sample.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+function foo() {
+    echo "bar"
+}
+
+echo "this is a sample bash script"
