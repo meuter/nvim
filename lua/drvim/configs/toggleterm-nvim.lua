3289a5a3ffd1544fbac4784e240f83a568ef3cc6
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-21 13:45:22 +0200

    feature: sane keyboard mapping in terminal

--- lua/drvim/configs/toggleterm-nvim.lua
+++ lua/drvim/configs/toggleterm-nvim.lua
@@ -13,3 +13,21 @@ require("toggleterm").setup {
     shell = vim.o.shell,
 }
 
+-- Map Esc and Ctrl+w in terminal mode like in normal mode
+vim.api.nvim_create_autocmd("TermOpen", {
+    callback = function()
+        local opts = { noremap = true }
+        vim.api.nvim_buf_set_keymap(0, 't', '<C-w>', [[<C-\><C-n><C-W>]], opts)
+        vim.api.nvim_buf_set_keymap(0, 't', '<Esc>', [[<C-\><C-n>]], opts)
+    end
+})
+
+-- When switching to terinal window, automatically enter insert mode
+vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+    callback = function()
+        if vim.bo.filetype == "toggleterm" then
+            vim.cmd("startinsert")
+        end
+    end
+})
+
