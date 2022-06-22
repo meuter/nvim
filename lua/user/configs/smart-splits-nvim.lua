006d512a151e73a1cfc8ccbe49a0fc13fc968580
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-22 12:02:43 +0200

    fix: the setup specs for smart-splits changed

--- lua/user/configs/smart-splits-nvim.lua
+++ lua/user/configs/smart-splits-nvim.lua
@@ -1,23 +1,4 @@
-require('smart-splits').setup({
-  -- Ignored filetypes (only while resizing)
-  ignored_filetypes = {
-    'nofile',
-    'quickfix',
-    'prompt',
-  },
-  -- Ignored buffer types (only while resizing)
-  ignored_buftypes = { 'NvimTree' },
-  -- when moving cursor between splits left or right,
-  -- place the cursor on the same row of the *screen*
-  -- regardless of line numbers. False by default.
-  -- Can be overridden via function parameter, see Usage.
-  move_cursor_same_row = false,
-  -- key to exit persistent resize mode
-  resize_mode_quit_key = '<ESC>',
-  -- set to true to silence the notifications
-  -- when entering/exiting persistent resize mode
-  resize_mode_silent = false,
-})
+require('smart-splits').setup {}
 
 -- resizing splits
 vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
@@ -30,4 +11,3 @@ vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
 vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
-
