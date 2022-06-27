fb99a6b9bed1fc6838023fb03fc03810dcc79f82
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-27 11:24:11 +0200

    feature: which-key now with rounded corners

--- lua/user/configs/which-key-nvim.lua
+++ lua/user/configs/which-key-nvim.lua
@@ -1,19 +1,35 @@
 local which_key = require("which-key")
 
+which_key.setup {
+    window = {
+        border = "double", -- none, single, double, shadow
+        position = "bottom", -- bottom, top
+        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+        winblend = 0
+    },
+    layout = {
+        height = { min = 4, max = 25 }, -- min and max height of the columns
+        width = { min = 20, max = 50 }, -- min and max width of the columns
+        spacing = 3, -- spacing between columns
+        align = "left", -- align columns left, center or right
+    },
+}
+
 which_key.register({
 
     f = {
         name = "file",
-        e = { "<cmd>NeoTreeFloatToggle<cr>", "File Explorer Panel"},
+        e = { "<cmd>NeoTreeFloatToggle<cr>", "File Explorer Panel" },
         f = { "<cmd>ProjectFiles<cr>", "Find File" },
         r = { "<cme>Telescope oldfiles<cr>", "Find Recent File" }
     },
     s = {
         name = "search",
         c = { "<cmd>Telescope neoclip<cr>", "Search Clipboard History" },
-        f = { "<cmd>SearchFunction<cr>", "Search Function"},
+        f = { "<cmd>SearchFunction<cr>", "Search Function" },
         l = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-        t = { "<cmd>Todo<cr>", "Search TODOs"},
+        t = { "<cmd>Todo<cr>", "Search TODOs" },
     },
     g = {
         name = "git",
@@ -28,12 +44,12 @@ which_key.register({
         i = { "<cmd>LspInstall<cr>", "Install Server" },
         l = { "<cmd>LspInstallInfo<cr>", "LSP List Servers" },
         r = { "<cmd>LspRestart<cr>", "Restart LSP Server" },
-        s = { "<cmd>LspInfo<cr>", "LSP Status"},
+        s = { "<cmd>LspInfo<cr>", "LSP Status" },
     },
     n = {
         name = "lsp",
         i = { "<cmd>Navigate<cr>", "Navigate Interactively" },
-        c = { "<cmd>cd ~/.config/nvim<cr>", "Navigate to Config"}
+        c = { "<cmd>cd ~/.config/nvim<cr>", "Navigate to Config" }
     }
 
     -- TODO(cme): make this modular dependin on what plugin is installed
