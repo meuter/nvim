fda015638beff7f3c8b58e9ff8be1510ed2c4de2
Cédric Meuter <cedric.meuter@gmail.com>
2022-07-24 08:48:18 +0200

    fix: treesitter update + markdown -> markdown_inline

--- lua/user/languages.lua
+++ lua/user/languages.lua
@@ -1,12 +1,12 @@
 return {
-    ["c"]           = "clangd",
-    ["python"]      = "pyright",
-    ["lua"]         = "sumneko_lua",
-    ["dockerfile"]  = "dockerls",
-    ["bash"]        = "bashls",
-    ["json"]        = "jsonls",
-    ["go"]          = "gopls"
+    ["c"]               = "clangd",
+    ["python"]          = "pyright",
+    ["lua"]             = "sumneko_lua",
+    ["dockerfile"]      = "dockerls",
+    ["bash"]            = "bashls",
+    ["json"]            = "jsonls",
+    ["go"]              = "gopls",
+    ["markdown_inline"] = "remark_ls"
     -- TODO(cme): support html, js, ts, react
     -- TODO(cme): support rust
 }
-
