bd070d4150869884a5d8647e13030acf6d47a197
Cédric Meuter <cedric.meuter@gmail.com>
2022-08-14 08:43:09 +0200

    feature: added 'CodeAction' command

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -9,8 +9,8 @@ command("CloseOthers", "%bd|e#|bd#", { desc="Close All Buffers Except Current"})
 command("ConvertHexStringLineToCArray", hexconvert.convert_hexstring_current_line_to_c_array, { desc="Converts Hex String to C Array" })
 command("ConvertHexStringSelectionToCArray", hexconvert.convert_hexstring_selection_to_c_array, { desc="Converts Hex String to C Array" })
 command("ToggleMouseCopy", toggle_mouse_copy, { desc="Toggle Mouse Copy"})
-
 command("Format", format.format, {desc="Format Code"})
 command("EnableFormatOnSave", format.enable_format_on_save, {desc="Enable Format on Save"})
 command("DisableFormatOnSave", format.disable_format_on_save, {desc="Disable Format on Save"})
+command("CodeAction", vim.lsp.buf.code_action, { desc="Execute LSP code action"})
 
