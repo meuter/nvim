9122aa47034f7988724728c2275e49626d7bdf73
Cédric Meuter <cedric.meuter@gmail.com>
2022-11-12 07:35:07 +0100

    setup: cleanup nvim-comment config file

--- lua/user/configs/nvim-comment.lua
+++ lua/user/configs/nvim-comment.lua
@@ -1,13 +1,9 @@
-require("nvim_comment").setup {
-    hook = function()
-        if vim.api.nvim_buf_get_option(0, "filetype") == "vue" then
-            require("ts_context_commentstring.internal").update_commentstring()
-        end
-    end
-}
+require("nvim_comment").setup{}
 
 -- Ctrl+/ to toggle comment
-vim.keymap.set("n", "<C-_>", "<CMD>CommentToggle<CR>j")
-vim.keymap.set("i", "<C-_>", "<C-\\><C-N><CMD>CommentToggle<CR>ji")
-vim.keymap.set("v", "<C-_>", ":'<,'>CommentToggle<CR>gv<esc>j")
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-_>", "<CMD>CommentToggle<CR>j", opts)
+vim.keymap.set("i", "<C-_>", "<C-\\><C-N><CMD>CommentToggle<CR>ji", opts)
+vim.keymap.set("v", "<C-_>", ":'<,'>CommentToggle<CR>gv<esc>j", opts)
+
 
