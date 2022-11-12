5b71ab2d1edfb4762de49e6f6754d71e953b36a6
Cédric Meuter <cedric.meuter@gmail.com>
2022-11-12 07:36:29 +0100

    setup: cleantup move-nvim config file

--- lua/user/configs/move-nvim.lua
+++ lua/user/configs/move-nvim.lua
@@ -1,9 +1,9 @@
--- Alt+Up/Down to move lines around
-vim.keymap.set("n", "<A-Down>", ":MoveLine(1)<CR>")
-vim.keymap.set("n", "<A-Up>",   ":MoveLine(-1)<CR>")
-vim.keymap.set("v", "<A-Down>", ":MoveBlock(1)<CR>")
-vim.keymap.set("v", "<A-Up>",   ":MoveBlock(-1)<CR>")
-vim.keymap.set("i", "<A-Down>", "<C-\\><C-N>:MoveLine(1)<CR>i")
-vim.keymap.set("i", "<A-Up>",   "<C-\\><C-N>:MoveLine(-1)<CR>i")
+local opts = { noremap = true, silent = true }
 
+vim.keymap.set("n", "<A-Down>", ":MoveLine(1)<CR>", opts)
+vim.keymap.set("n", "<A-Up>",   ":MoveLine(-1)<CR>", opts)
+vim.keymap.set("v", "<A-Down>", ":MoveBlock(1)<CR>", opts)
+vim.keymap.set("v", "<A-Up>",   ":MoveBlock(-1)<CR>", opts)
+vim.keymap.set("i", "<A-Down>", "<C-\\><C-N>:MoveLine(1)<CR>i", opts)
+vim.keymap.set("i", "<A-Up>",   "<C-\\><C-N>:MoveLine(-1)<CR>i", opts)
 
