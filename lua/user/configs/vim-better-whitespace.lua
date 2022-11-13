21052bba4a4f8253df9cf8a0391b341990ddf83e
Cédric Meuter <cedric.meuter@gmail.com>
2022-11-13 07:41:53 +0100

    feature: disable whitespace hightlight in certain buffer types

--- lua/user/configs/vim-better-whitespace.lua
+++ lua/user/configs/vim-better-whitespace.lua
@@ -2,3 +2,16 @@ vim.g.better_whitespace_enabled = 1
 vim.g.strip_whitespace_on_save = 1
 vim.g.strip_whitespace_confirm = 0
 
+vim.g.better_whitespace_filetypes_blacklist={
+    "terminal",
+    "toggleterm",
+    "diff",
+    "git",
+    "gitcommit",
+    "unite",
+    "qf",
+    "help",
+    "markdown",
+    "fugitive"
+}
+
