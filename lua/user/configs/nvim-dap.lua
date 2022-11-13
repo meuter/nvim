e657f4a5bec247fba31e438b349ba8a243cdf9aa
Cédric Meuter <cedric.meuter@gmail.com>
2022-11-13 09:41:09 +0100

    feature: added support for python, go, rust and cpp DAP debugging

--- lua/user/configs/nvim-dap.lua
+++ lua/user/configs/nvim-dap.lua
@@ -1,30 +1,81 @@
-local dap = require('dap')
+local dap = require("dap")
 
-dap.adapters.lldb = {
-    type = 'executable',
-    command = 'lldb-vscode',
-    name = 'lldb'
+dap.adapters.codelldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+        command = "codelldb",
+        args = { "--port", "${port}" },
+    }
+}
+dap.adapters.python = {
+    type = "executable";
+    command = "debugpy-adapter";
+}
+dap.adapters.delve = {
+    type = 'server',
+    port = '${port}',
+    executable = {
+        command = 'dlv',
+        args = { 'dap', '-l', '127.0.0.1:${port}' },
+    }
 }
 
 dap.configurations.c = {
     {
-        name = 'Launch',
-        type = 'lldb',
-        request = 'launch',
+        name = "Launch",
+        type = "codelldb",
+        request = "launch",
         program = function()
-            -- TODO(cme): find executable in the workspace using Telescope
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
         end,
-        cwd = '${workspaceFolder}',
+        cwd = "${workspaceFolder}",
         stopOnEntry = false,
         args = {},
     }
 }
-
 dap.configurations.cpp = dap.configurations.c
+dap.configurations.rust = dap.configurations.c
 
+dap.configurations.python = {
+    {
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+                return cwd .. "/venv/bin/python"
+            elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+                return cwd .. "/.venv/bin/python"
+            else
+                return "/usr/bin/python"
+            end
+        end,
+    },
+}
 
--- TODO(cme): add support for go debugging
--- TODO(cme): add support for rust debugging
--- TODO(cme): add support for python debugging
-
+dap.configurations.go = {
+    {
+        type = "delve",
+        name = "Debug",
+        request = "launch",
+        program = "${file}"
+    },
+    {
+        type = "delve",
+        name = "Debug test", -- configuration for debugging test files
+        request = "launch",
+        mode = "test",
+        program = "${file}"
+    },
+    -- works with go.mod packages and sub packages
+    {
+        type = "delve",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}"
+    }
+}
--- lua/user/languages.lua
+++ lua/user/languages.lua
@@ -1,12 +1,12 @@
 return {
-    ["c"]               = { "clangd" },
-    ["python"]          = { "pyright" },
+    ["c"]               = { "clangd", "codelldb" },
+    ["python"]          = { "pyright", "debugpy" },
     ["lua"]             = { "lua-language-server" },
     ["dockerfile"]      = { "dockerfile-language-server" },
     ["bash"]            = { "bash-language-server" },
     ["json"]            = { "json-lsp" },
-    ["go"]              = { "gopls" },
+    ["go"]              = { "gopls", "delve" },
     ["markdown_inline"] = { "remark-language-server" },
     ["cmake"]           = { "cmake-language-server" },
-    ["rust"]            = { "rust-analyzer" },
+    ["rust"]            = { "rust-analyzer", "codelldb" },
 }
