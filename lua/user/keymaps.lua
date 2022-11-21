e533f4cfa787cd5cc623d24761a300617d003319
Cédric Meuter <cedric.meuter@gmail.com>
2022-11-21 09:56:39 +0100

    feat: esc x3 to dismiss search highlight

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,18 +1,29 @@
+-- Functional wrapper for mapping custom keybindings
+function map(mode, lhs, rhs)
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
+
 -- Tab/Shift+tab to indent/dedent
-vim.keymap.set("v", "<Tab>", ">gv")
-vim.keymap.set("n", "<Tab>", "v><C-\\><C-N>")
-vim.keymap.set("v", "<S-Tab>", "<gv")
-vim.keymap.set("n", "<S-Tab>", "v<<C-\\><C-N>")
-vim.keymap.set("i", "<S-Tab>", "<C-\\><C-N>v<<C-\\><C-N>^i")
+map("v", "<Tab>", ">gv")
+map("n", "<Tab>", "v><C-\\><C-N>")
+map("v", "<S-Tab>", "<gv")
+map("n", "<S-Tab>", "v<<C-\\><C-N>")
+map("i", "<S-Tab>", "<C-\\><C-N>v<<C-\\><C-N>^i")
 
 -- Ctrl+Left/Right use word boundary
-vim.keymap.set("n", "<C-Left>", "b")
-vim.keymap.set("n", "<C-Right>", "w")
+map("n", "<C-Left>", "b")
+map("n", "<C-Right>", "w")
 
 -- Alt+Left/Right to navigate jumplist
-vim.keymap.set("n", "<A-Left>", "<C-O>")
-vim.keymap.set("n", "<A-Right>", "<C-I>")
+map("n", "<A-Left>", "<C-O>")
+map("n", "<A-Right>", "<C-I>")
 
 -- Ctrl+w in insert mode automatically escpaces
-vim.keymap.set("i", "<C-w>", "<C-\\><C-N><C-w>")
+map("i", "<C-w>", "<C-\\><C-N><C-w>")
+
+-- dismiss search highlight with esc x 3
+map("n", "<esc><esc><esc>", "<CMD>nohl<CR>")
+map("v", "<esc><esc><esc>", "<CMD>nohl<CR>")
+map("i", "<esc><esc><esc>", "<C-\\><C-N><CMD>nohl<CR>")
 
