1214ba86cc6a2b06edaa135f8d8706421f95453f
Cédric Meuter <cedric.meuter@gmail.com>
2022-12-26 11:24:46 +0100

    merged mason into the lsp module

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,6 +26,24 @@ function trouble.config()
 end
 
 -------------------------------------------------------------------------------
+-- mason
+-------------------------------------------------------------------------------
+local mason = {
+    "williamboman/mason.nvim",
+    commit = "60a03f55ab8b232c56c9ce3c4c3bf5673847e245"
+}
+
+function mason.config()
+    require("mason").setup {
+        ui = { border = "single" }
+    }
+end
+
+function mason.build()
+    require("plugins.languages").on_mason_install()
+end
+
+-------------------------------------------------------------------------------
 -- null-ls
 -------------------------------------------------------------------------------
 local nullls = {
@@ -152,6 +170,7 @@ end
 -------------------------------------------------------------------------------
 return {
     trouble,
+    mason,
     nullls,
     lspzero,
 }
