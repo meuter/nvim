709122f22b4d84fb75bc7112414ad6198bd1410d
Cédric Meuter <cedric.meuter@gmail.com>
2022-12-26 13:26:42 +0100

    unpin mason, the UI change was deliberate

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,7 +30,6 @@ end
 -------------------------------------------------------------------------------
 local mason = {
     "williamboman/mason.nvim",
-    commit = "60a03f55ab8b232c56c9ce3c4c3bf5673847e245"
 }
 
 function mason.config()
