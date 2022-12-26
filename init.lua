3d50fb1ef1a3658086cccfd13caf4e9410c695d5
Cédric Meuter <cedric.meuter@gmail.com>
2022-12-26 08:57:39 +0100

    added more comments in init.lua

--- init.lua
+++ init.lua
@@ -1,24 +1,33 @@
 -------------------------------------------------------------------------------
 -- options
 -------------------------------------------------------------------------------
+-- backup, swap and undo
 vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.writebackup = false
 vim.opt.undofile = true
 vim.opt.autoread = true
+
+-- indent
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+
+-- search
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.incsearch = true
 vim.opt.wrapscan = true
 vim.opt.smartcase = true
+
+-- sign column
 vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.numberwidth = 4
 vim.opt.signcolumn = "yes"
+
+-- display hidden character
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("lead:⋅")
@@ -27,11 +36,17 @@ vim.opt.listchars:append("eol:")
 vim.opt.listchars:append("tab: ")
 vim.opt.fillchars:append("diff: ")
 vim.opt.fillchars:append("eob: ")
+
+-- status and tab bar
 vim.opt.cmdheight = 0
 vim.opt.laststatus = 3
 vim.opt.showtabline = 0
+
+-- completion menu
 vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.pumheight = 10
+
+-- misc
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.clipboard = "unnamedplus"
@@ -39,26 +54,31 @@ vim.opt.cursorline = true
 vim.opt.foldenable = false
 vim.opt.wrap = false
 vim.opt.updatetime = 100
-vim.opt.shortmess:append("ca")
 vim.opt.shell = "/bin/bash"
 
 -------------------------------------------------------------------------------
 -- colors
 -------------------------------------------------------------------------------
-vim.opt.termguicolors = true
 
 local float_bg = "#272727"
+local diff_green = "#044025"
+local diff_red = "#400404"
+local diff_yellow = "#403d04"
 local visual_selected = "#253747"
 
+-- enable true colors
+vim.opt.termguicolors = true
+
+-- start with a builtin color scheme
 vim.cmd("colorscheme habamax")
 
 -- fixup some general highlight
 vim.api.nvim_set_hl(0, "VertSplit", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "Visual", { fg = "NONE", bg = visual_selected })
-vim.api.nvim_set_hl(0, "DiffAdd", { fg = "NONE", bg = "#044025" })
-vim.api.nvim_set_hl(0, "DiffChange", { fg = "NONE", bg = "#044025" })
-vim.api.nvim_set_hl(0, "DiffDelete", { fg = "NONE", bg = "#400404" })
-vim.api.nvim_set_hl(0, "DiffText", { fg = "NONE", bg = "#403d04" })
+vim.api.nvim_set_hl(0, "DiffAdd", { fg = "NONE", bg = diff_green })
+vim.api.nvim_set_hl(0, "DiffChange", { fg = "NONE", bg = diff_green })
+vim.api.nvim_set_hl(0, "DiffDelete", { fg = "NONE", bg = diff_red })
+vim.api.nvim_set_hl(0, "DiffText", { fg = "NONE", bg = diff_yellow })
 vim.api.nvim_set_hl(0, "Pmenu", { fg = "NONE", bg = float_bg })
 vim.api.nvim_set_hl(0, "PmenuSel", { fg = "NONE", bg = visual_selected })
 vim.api.nvim_set_hl(0, "FloatBorder", { fg = "NONE", bg = float_bg })
@@ -103,10 +123,34 @@ vim.keymap.set("i", "<C-w>", "<C-\\><C-n><C-w>")
 -- dismiss search highlight with esc
 vim.keymap.set({ "n", "v" }, "<ESC>", "<CMD>nohl<CR><ESC>")
 
+
+-------------------------------------------------------------------------------
+-- commands
+-------------------------------------------------------------------------------
+
+-- always open help windows in full screen
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = vim.api.nvim_create_augroup("OpenHelpInFullWindow", { clear = true }),
+    callback = function()
+        if vim.bo.filetype == "help" then
+            local key = vim.api.nvim_replace_termcodes("<C-w>o", true, false, true)
+            vim.api.nvim_feedkeys(key, "n", false)
+        end
+    end,
+})
+
+-- with autoread set, this forces a check of changed files when focus changes
+-- (useful when using space age sed to search and replace behing vim's back)
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+    group = vim.api.nvim_create_augroup("CheckReloadOnFocusChange", { clear = true }),
+    command = "checktime",
+})
+
 -------------------------------------------------------------------------------
 -- plugins
 -------------------------------------------------------------------------------
 
+-- automatically bootstrap package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -119,6 +163,8 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.runtimepath:prepend(lazypath)
+
+-- load all plugins
 require("lazy").setup("plugins", {
     ui = { border = "single" },
 })
