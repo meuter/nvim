cef3f7e42d24db0576ae2178a5349638ba9207e1
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-19 17:38:06 +0100

    mason install is run on each installed - need to find a better way

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# To Do
+
+- mouse menu with single border
+- add clippy support for rust
+- command to convert hex digits to bytes
+- https://github.com/mfussenegger/nvim-dap-python
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,29 +6,29 @@ return {
                 border = "single"
             }
         }
-        require("mason.api.command").MasonInstall {
-            "bash-language-server",
-            "clangd",
-            "cmake-language-server",
-            "codelldb",
-            "dockerfile-language-server",
-            "gopls",
-            "delve",
-            "json-lsp",
-            "lua-language-server",
-            "marksman",
-            "pyright",
-            "black",
-            "debugpy",
-            "rust-analyzer",
-            "codelldb",
-            "taplo",
-            "typescript-language-server",
-            "eslint-lsp",
-            "html-lsp",
-            "emmet-ls",
-            "css-lsp",
-            "yaml-language-server"
-        }
+        -- require("mason.api.command").MasonInstall {
+        --     "bash-language-server",
+        --     "clangd",
+        --     "cmake-language-server",
+        --     "codelldb",
+        --     "dockerfile-language-server",
+        --     "gopls",
+        --     "delve",
+        --     "json-lsp",
+        --     "lua-language-server",
+        --     "marksman",
+        --     "pyright",
+        --     "black",
+        --     "debugpy",
+        --     "rust-analyzer",
+        --     "codelldb",
+        --     "taplo",
+        --     "typescript-language-server",
+        --     "eslint-lsp",
+        --     "html-lsp",
+        --     "emmet-ls",
+        --     "css-lsp",
+        --     "yaml-language-server"
+        -- }
     end
 }
