8bbd76d52892f2029c9f4d1e198f360142cd2996
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-20 08:20:52 +0100

    lazy load barbecue

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -3,6 +3,7 @@ return {
     name = "barbecue",
     -- https://github.com/utilyre/barbecue.nvim/issues/61
     branch = "fix/E36",
+    event = "BufRead *",
     dependencies = {
         "SmiteshP/nvim-navic",
         "nvim-tree/nvim-web-devicons",
