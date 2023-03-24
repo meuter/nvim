18d522337b9cce8d078ca43480a8ea8d02d3c331
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-24 11:27:29 +0100

    fixed dressing border on vim.select()

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -2,5 +2,15 @@ return {
     "stevearc/dressing.nvim",
     opts = {
         input = { border = "single" },
+        select = {
+            telescope = require('telescope.themes').get_dropdown {
+                previewer = false,
+                borderchars = {
+                    prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
+                    results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
+                    preview = { "─", "│", "─", "│", "╭", "╮", "┘", "└" },
+                },
+            }
+        }
     }
 }
