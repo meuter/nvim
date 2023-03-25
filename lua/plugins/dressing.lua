efc06f6ad69478b1cffd62f7e67649a2d841c25a
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-25 14:44:12 +0100

    fixed dressing plugin specs - dependency on telescope was note explicit

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,16 +1,21 @@
 return {
     "stevearc/dressing.nvim",
-    opts = {
-        input = { border = "single" },
-        select = {
-            telescope = require('telescope.themes').get_dropdown {
-                previewer = false,
-                borderchars = {
-                    prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
-                    results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
-                    preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-                },
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        require("dressing").setup {
+            input = { border = "single" },
+            select = {
+                telescope = require('telescope.themes').get_dropdown {
+                    previewer = false,
+                    borderchars = {
+                        prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
+                        results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
+                        preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+                    },
+                }
             }
         }
-    }
+    end
 }
