a236df5038d2ded9da6d5cdc909bf7210b73dfab
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-26 09:52:46 +0200

    trying to untangle lsp-zero from cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,82 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
+        "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        cmp.setup({
+            sources = {
+                { name = "path" },
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+                { name = "luasnip" },
+            },
+            mappings = {
+                ['<CR>'] = cmp.mapping.confirm { select = false },
+                ['<Up>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+                ['<Down>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            },
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            completion = {
+                completeopt = "menu,menuone,noselect,noinsert"
+            },
+            window = {
+                completion = { border = "single" },
+                documentation = { border = "single" },
+            },
+            formatting = {
+                fields = { "kind", "abbr", "menu" },
+                format = require("lspkind").cmp_format({
+                    mode = "symbol",
+                    maxwidth = 50,
+                    ellipsis_char = "...",
+                }),
+            },
+        })
+        cmp.setup.cmdline("/", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "buffer", keyword_length = 3 },
+            },
+        })
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "path",    keyword_length = 3 },
+                { name = "cmdline", keyword_length = 3 },
+            },
+        })
+    end
+}
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -3,24 +3,16 @@ return {
     dependencies = {
         "neovim/nvim-lspconfig",
         "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
         "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-cmdline",
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
         "L3MON4D3/LuaSnip",
-        "rafamadriz/friendly-snippets",
         "lukas-reineke/lsp-format.nvim",
-        "onsails/lspkind.nvim",
         "b0o/schemastore.nvim",
     },
     config = function()
         local lsp = require("lsp-zero").preset({
             name = "recommended",
             suggest_lsp_servers = false,
+            manage_nvim_cmp = false,
             sign_icons = {
                 error = "",
                 warn = "",
@@ -58,39 +50,6 @@ return {
         })
 
         lsp.setup()
-
-        -- override nvim-cmp configuration
-        local cmp = require("cmp")
-        cmp.setup(lsp.defaults.cmp_config {
-            completion = {
-                completeopt = "menu,menuone,noselect,noinsert"
-            },
-            window = {
-                completion = { border = "single" },
-                documentation = { border = "single" },
-            },
-            formatting = {
-                fields = { "kind", "abbr", "menu" },
-                format = require("lspkind").cmp_format({
-                    mode = "symbol",
-                    maxwidth = 50,
-                    ellipsis_char = "...",
-                }),
-            },
-        })
-        cmp.setup.cmdline("/", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer", keyword_length = 3 },
-            },
-        })
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "path",    keyword_length = 3 },
-                { name = "cmdline", keyword_length = 3 },
-            },
-        })
 
         -- tweak lsp UI
         vim.diagnostic.config { virtual_text = true }
