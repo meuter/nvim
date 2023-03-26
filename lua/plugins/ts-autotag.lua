341e741ace5f06f309e02c1c4d80c61348fbf28d
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-26 08:33:45 +0200

    added nvim-ts-autotag for html

---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,9 @@
+return {
+    "windwp/nvim-ts-autotag",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("nvim-ts-autotag").setup()
+    end
+}
