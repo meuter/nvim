c5ff6a1ca38f853f937690711a8586d041a0f542
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-26 08:39:48 +0200

    cleanup mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,34 +1,15 @@
 return {
     "williamboman/mason.nvim",
-    config = function()
-        require("mason").setup {
-            ui = {
-                border = "single"
-            }
+    cmd = {
+        "Mason",
+        "MasonInstall",
+        "MasonUninstall",
+        "MasonUninstallAll",
+        "MasonLog",
+    },
+    opts = {
+        ui = {
+            border = "single"
         }
-        -- require("mason.api.command").MasonInstall {
-        --     "bash-language-server",
-        --     "clangd",
-        --     "cmake-language-server",
-        --     "codelldb",
-        --     "dockerfile-language-server",
-        --     "gopls",
-        --     "delve",
-        --     "json-lsp",
-        --     "lua-language-server",
-        --     "marksman",
-        --     "pyright",
-        --     "black",
-        --     "debugpy",
-        --     "rust-analyzer",
-        --     "codelldb",
-        --     "taplo",
-        --     "typescript-language-server",
-        --     "eslint-lsp",
-        --     "html-lsp",
-        --     "emmet-ls",
-        --     "css-lsp",
-        --     "yaml-language-server"
-        -- }
-    end
+    }
 }
