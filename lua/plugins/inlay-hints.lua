4918c0d669dfb253c721beb5c99234287979ada4
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-30 08:26:09 +0200

    added inlay-hints for lua

---
+++ lua/plugins/inlay-hints.lua
@@ -0,0 +1,6 @@
+return {
+    "simrat39/inlay-hints.nvim",
+    opts = {
+        renderer = "inlay-hints/render/eol"
+    }
+}
--- lua/plugins/neodev.lua
+++ lua/plugins/neodev.lua
@@ -2,6 +2,7 @@ return {
     "VonHeikemen/lsp-zero.nvim",
     dependencies = {
         "folke/neodev.nvim",
+        "simrat39/inlay-hints.nvim",
     },
     opts = function(_, opts)
         require("plugins.lsp-zero").on_setup(opts, function(lsp)
@@ -11,12 +12,16 @@ return {
                     runtime = false
                 }
             }
-            lsp.configure("sumneko_lua", {
+            lsp.configure("lua_ls", {
+                on_attach = function(client, buffer)
+                    require("inlay-hints").on_attach(client, buffer)
+                end,
                 settings = {
                     Lua = {
                         diagnostics = { globals = { "vim" } },
                         telemetry = { enable = false },
                         workspace = { checkThirdParty = false },
+                        hint = { enable = true }
                     },
                 }
             })
