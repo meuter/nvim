eb08ea98d96c7a25563875358726f65f6bbdd94d
Cédric Meuter <cedric.meuter@gmail.com>
2023-04-21 05:57:54 +0200

    simplified tab/s-tab keymap

--- init.lua
+++ init.lua
@@ -71,11 +71,11 @@ vim.api.nvim_create_autocmd("User", {
 -------------------------------------------------------------------------------
 
 -- Tab/Shift+tab to indent/dedent
+vim.keymap.set("n", "<Tab>", ">>")
+vim.keymap.set("n", "<S-Tab>", "<<")
 vim.keymap.set("v", "<Tab>", ">gv")
-vim.keymap.set("n", "<Tab>", "v><C-\\><C-N>")
 vim.keymap.set("v", "<S-Tab>", "<gv")
-vim.keymap.set("n", "<S-Tab>", "v<<C-\\><C-N>")
-vim.keymap.set("i", "<S-Tab>", "<C-\\><C-N>v<<C-\\><C-N>^i")
+vim.keymap.set("i", "<S-Tab>", "<C-\\><C-N><<<C-\\><C-N>^i")
 
 -- Ctrl+Left/Right use word boundary
 vim.keymap.set("n", "<C-Left>", "b")
