eb965c9eb6d41b805db9cd72b885412c0e1114a0
Cédric Meuter <cedric.meuter@gmail.com>
2023-09-14 08:46:36 +0200

    fixed clangd cmd invokation

--- lua/plugins/clangd_extensions.lua
+++ lua/plugins/clangd_extensions.lua
@@ -3,23 +3,26 @@ return {
     ft = { "c", "cpp" },
     dependencies = {
         "VonHeikemen/lsp-zero.nvim",
+        "neovim/nvim-lspconfig",
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        require("clangd_extensions").setup {
-            server = require("lsp-zero").build_options("clangd", {
-                cmd = {
-                    "clangd",
-                    "-j=4",
-                    "--clang-tidy",
-                    "--background-index",
-                    "--header-insertion=never",
-                    "--fallback-style=webkit",
-                    "--all-scopes-completion",
-                    "--completion-style=detailed",
-                    "--pch-storage=memory",
-                },
-            })
-        }
+        local server_options = require("lsp-zero").build_options("clangd", {
+            cmd = {
+                "clangd",
+                "-j=4",
+                "--clang-tidy",
+                "--background-index",
+                "--header-insertion=never",
+                "--fallback-style=webkit",
+                "--all-scopes-completion",
+                "--completion-style=detailed",
+                "--pch-storage=memory",
+            }
+        })
+        require("lspconfig").clangd.setup(server_options)
+        require("clangd_extensions").setup()
+        require("clangd_extensions.inlay_hints").setup_autocmd()
+        require("clangd_extensions.inlay_hints").set_inlay_hints()
     end
 }
