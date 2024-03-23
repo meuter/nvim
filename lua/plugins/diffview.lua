5616b350a3ac64add3c1c76bb27ef0e482987fd4
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-23 09:11:57 +0100

    disable fold in diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -21,6 +21,17 @@ return {
     config = function()
         local actions = require("diffview.config").actions
         require("diffview").setup {
+            enhanced_diff_hl = true,
+            hooks = {
+                diff_buf_read = function()
+                    vim.opt_local.foldlevel = 99
+                    vim.opt_local.foldenable = false
+                end,
+                diff_buf_win_enter = function()
+                    vim.opt_local.foldlevel = 99
+                    vim.opt_local.foldenable = false
+                end
+            },
             keymaps = {
                 view = {
                     ["<C-b>"] = actions.toggle_files,
