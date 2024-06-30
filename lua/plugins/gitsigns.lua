c58b0b354a4a26f79814ef3757abd666fc8af95d
Cédric Meuter <cedric.meuter@gmail.com>
2024-06-30 08:57:18 +0200

    fix: deprecation in gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,22 +1,13 @@
-local function sign(op, char)
-    return {
-        hl = "GitSigns" .. op,
-        text = char,
-        numhl = "GitSigns" .. op .. "Nr",
-        linehl = "GitSigns" .. op .. "Ln",
-    }
-end
-
 return {
     "lewis6991/gitsigns.nvim",
     opts = {
         signs = {
-            add = sign("Add", "▎"),
-            untracked = sign("Add", "▎"),
-            change = sign("Change", "▎"),
-            changedelete = sign("Change", "▎"),
-            delete = sign("Delete", "▎"),
-            topdelete = sign("Delete", "▎"),
+            add = { text = "▎" },
+            untracked = { text = "▎" },
+            change = { text = "▎" },
+            changedelete = { text = "▎" },
+            delete = { text = "▎" },
+            topdelete = { text = "▎" },
         },
     }
 }
