6cedab86d2f38baeb3dd2ce5d3d06f8428a9fbe9
Cédric Meuter <cedric.meuter@gmail.com>
2024-10-04 09:03:07 +0200

    TroubleToggle -> Trouble diagnostics toggle

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,7 +1,7 @@
 return {
     "folke/trouble.nvim",
     keys = {
-        { "<F10>", "<CMD>TroubleToggle<CR>", mode = { "n", "v", "i" } }
+        { "<F10>", "<CMD>Trouble diagnostics toggle<CR>", mode = { "n", "v", "i" } }
     },
     dependencies = {
         "nvim-tree/nvim-web-devicons",
