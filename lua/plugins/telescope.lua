045fd41d97c5edca94de7c51c998c998d4ee683f
Cédric Meuter <cedric.meuter@gmail.com>
2024-10-21 13:32:28 +0200

    telescope: simplify config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,65 +1,30 @@
 return {
     "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    event = "VimEnter",
     keys = {
-        { "<C-t>", "<CMD>Telescope<CR>",                                                       mode = { "n", "v", "i" } },
-        { "<C-p>", "<CMD>Telescope find_files<CR>",                                            mode = { "n", "v", "i" } },
-        { "<A-d>", "<CMD>Telescope diagnostics<CR>",                                           mode = { "n", "v", "i" } },
-        { "<A-p>", "<CMD>Telescope buffers<CR>",                                               mode = { "n", "v", "i" } },
-        { "<C-l>", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", mode = { "n", "v", "i" } },
-        { "<C-o>", "<CMD>Telescope lsp_document_symbols<CR>",                                  mode = { "n", "v", "i" } },
-        { "<C-s>", "<CMD>Telescope grep_string<CR>",                                           mode = { "n", "i" } },
-    },
-    cmd = {
-        "Telescope",
+        { "<C-t>", "<CMD>Telescope<CR>",             mode = { "n", "v", "i" } },
+        { "<C-p>", "<CMD>Telescope find_files<CR>",  mode = { "n", "v", "i" } },
+        { "<A-d>", "<CMD>Telescope diagnostics<CR>", mode = { "n", "v", "i" } },
+        { "<A-p>", "<CMD>Telescope buffers<CR>",     mode = { "n", "v", "i" } },
+        { "<C-l>", "<CMD>Telescope live_grep<CR>",   mode = { "n", "v", "i" } },
+        { "<C-s>", "<CMD>Telescope grep_string<CR>", mode = { "n", "i" } },
     },
     dependencies = {
-        { "nvim-lua/plenary.nvim" },
-        { "nvim-telescope/telescope-fzf-native.nvim",    build = "make" },
-        { "nvim-telescope/telescope-live-grep-args.nvim" },
-        { "nvim-telescope/telescope-dap.nvim" },
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        { "nvim-telescope/telescope-ui-select.nvim" },
+        { "nvim-tree/nvim-web-devicons" },
     },
     config = function()
-        local telescope = require("telescope")
-        local lga_actions = require("telescope-live-grep-args.actions")
-
-        telescope.setup {
-            defaults = {
-                prompt_prefix = "🔎 ",
-                color_devicons = true,
-                selection_caret = "❯ ",
-                preview = {
-                    treesitter = true,
-                },
-                borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-            },
-            pickers = {
-                colorscheme = {
-                    enable_preview = true,
-                },
-                buffers = {
-                    theme = "dropdown",
-                    previewer = false,
-                    borderchars = {
-                        prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
-                        results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
-                        preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-                    },
-                }
-            },
+        require("telescope").setup({
             extensions = {
-                live_grep_args = {
-                    auto_quoting = true,
-                    mappings = {
-                        i = {
-                            ["<C-k>"] = lga_actions.quote_prompt(),
-                            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-                        },
-                    },
-                }
-            }
-        }
-        telescope.load_extension("fzf")
-        telescope.load_extension("live_grep_args")
-        telescope.load_extension("dap")
-    end
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown(),
+                },
+            },
+        })
+        require("telescope").load_extension("fzf")
+        require("telescope").load_extension("ui-select")
+    end,
 }
