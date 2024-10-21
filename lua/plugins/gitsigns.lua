93bd338cef69322f2ea3d8cc76dba9dcf3d13c8e
Cédric Meuter <cedric.meuter@gmail.com>
2024-10-21 13:24:54 +0200

    gitsigns: added keymap to preview hunk and changed sign symbol

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,13 +1,17 @@
 return {
     "lewis6991/gitsigns.nvim",
+    event = "VimEnter",
+    keys = {
+        { "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>", mode = { "n", "v" } },
+    },
     opts = {
         signs = {
-            add = { text = "▎" },
-            untracked = { text = "▎" },
-            change = { text = "▎" },
-            changedelete = { text = "▎" },
-            delete = { text = "▎" },
-            topdelete = { text = "▎" },
+            add = { text = "┃" },
+            change = { text = "┃" },
+            delete = { text = "┃" },
+            topdelete = { text = "┃" },
+            changedelete = { text = "┃" },
+            untracked = { text = "┃" },
         },
-    }
+    },
 }
