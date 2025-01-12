13f90bfa56b8db1613425d2bbc0774e6b5fdafab
Cédric Meuter <cedric.meuter@gmail.com>
2025-01-12 09:08:36 +0100

    added dropbar since barbecue in not maintained anymore

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,8 @@
+return {
+    'Bekaboo/dropbar.nvim',
+    config = function()
+        local dropbar_api = require('dropbar.api')
+        vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+        vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end
+}
