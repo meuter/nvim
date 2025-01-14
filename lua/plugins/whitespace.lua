b94d66f0afca67b32a998640c194122799604d4b
Cédric Meuter <cedric.meuter@gmail.com>
2025-01-14 07:47:55 +0100

    disable highlighting trailing whitespace, just remove them on save

--- lua/plugins/whitespace.lua
+++ lua/plugins/whitespace.lua
@@ -1,7 +1,7 @@
 return {
     "ntpeters/vim-better-whitespace",
     config = function()
-        vim.g.better_whitespace_enabled = 1
+        vim.g.better_whitespace_enabled = 0
         vim.g.strip_whitespace_on_save = 1
         vim.g.strip_whitespace_confirm = 0
         vim.g.better_whitespace_filetypes_blacklist = {
