ee0943549c9b8ed742263e9242e396d39c5820eb
Cédric Meuter <cedric.meuter@gmail.com>
2025-01-15 13:15:50 +0100

    tweak blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,17 +6,21 @@ return {
     version = "*",
     build = "cargo build --release",
     opts = {
-        -- "default" for mappings similar to built-in completion
-        -- "super-tab" for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- "enter" for mappings similar to "super-tab" but with "enter" to accept
-        -- See the full "keymap" documentation for information on defining your own keymap.
         keymap = {
-            preset = "super-tab"
+            preset = "enter"
         },
 
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = "mono"
+        },
+
+        completion = {
+            menu = {
+                auto_show = function(ctx)
+                    return ctx.mode ~= "cmdline" or not vim.tbl_contains({ '/', '?' }, vim.fn.getcmdtype())
+                end,
+            }
         },
 
         sources = {
