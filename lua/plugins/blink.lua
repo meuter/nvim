3caa8c3a30c288d7080d7a0d8e789b5cd5ea2f6a
Cédric Meuter <cedric.meuter@gmail.com>
2025-01-19 06:06:18 +0100

    switch back to enter mode

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -11,7 +11,7 @@ return {
         -- "enter" for mappings similar to "super-tab" but with "enter" to accept
         -- See the full "keymap" documentation for information on defining your own keymap.
         keymap = {
-            preset = "super-tab"
+            preset = "enter"
         },
 
         appearance = {
