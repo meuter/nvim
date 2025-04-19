b6024130bad4abf709f381f4892523dd214622e7
Cédric Meuter <cedric.meuter@gmail.com>
2025-04-19 10:55:36 +0200

    removed unused key bindings

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,8 +1,4 @@
 return {
     'Bekaboo/dropbar.nvim',
-    config = function()
-        local dropbar_api = require('dropbar.api')
-        vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-        vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
-    end
+    config = true
 }
