69b555b85779d867e1200095577be180ee266ce2
Cédric Meuter <cedric.meuter@gmail.com>
2025-04-19 10:53:30 +0200

    use default preset in blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,12 +6,8 @@ return {
     version = "*",
     build = "cargo build --release",
     opts = {
-        -- "default" for mappings similar to built-in completion
-        -- "super-tab" for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- "enter" for mappings similar to "super-tab" but with "enter" to accept
-        -- See the full "keymap" documentation for information on defining your own keymap.
         keymap = {
-            preset = "enter"
+            preset = "default"
         },
 
         appearance = {
