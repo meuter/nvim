b68daaa58d6bb100d826f7dbdbdff20d8158ae2e
Cédric Meuter <cedric.meuter@gmail.com>
2025-04-19 10:55:57 +0200

    cleanup lsp config from wincmd nonsense, no needed anymore

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,10 @@
+---@module 'snacks'
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         { "j-hui/fidget.nvim",   opts = {} },
-        "hrsh7th/cmp-nvim-lsp",
         { "mrcjkb/rustaceanvim", version = '^5', lazy = false }
     },
     config = function()
@@ -34,9 +34,6 @@ return {
             end,
         })
 
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
         vim.diagnostic.config({
             signs = {
                 text = {
@@ -49,9 +46,7 @@ return {
         })
 
         local servers = {
-            bashls = {
-                wincmd = "bash-language-server/node_modules/.bin/bash-language-server"
-            },
+            bashls = {},
             clangd = {
                 settings = {
                     clangd = {
@@ -75,7 +70,6 @@ return {
                     "--completion-style=detailed",
                     "--pch-storage=memory",
                 },
-                wincmd = "clangd/clangd_19.1.2/bin/clangd.exe",
             },
             docker_compose_language_service = {},
             dockerls = {},
@@ -89,24 +83,17 @@ return {
                         hint = { enable = true },
                     },
                 },
-                wincmd = "lua-language-server/bin/lua-language-server.exe",
             },
             neocmake = {
                 cmd = { "neocmakelsp", "--stdio" },
-                wincmd = "neocmakelsp/bin/neocmakelsp.exe"
             },
             pyright = {},
             rust_analyzer = {},
-            glsl_analyzer = {
-                wincmd = "glsl_analyzer/bin/glsl_analyzer.exe"
-            },
-            zls = {
-                wincmd = "zls/zls.exe"
-            },
+            glsl_analyzer = {},
+            zls = {},
         }
 
         local ensure_installed = vim.tbl_keys(servers or {})
-        local is_windows = vim.fn.has("win32") or vim.fn.has("win64")
 
         require("mason").setup()
         require("mason-lspconfig").setup({
@@ -117,21 +104,8 @@ return {
                     if server_name == "rust_analyzer" then
                         return -- already done by rustaceans
                     end
-
                     local server = servers[server_name] or {}
-
-                    if is_windows ~= 0 and server.wincmd ~= nil then
-                        -- NOTE: on windows, mason tries to run CMD script but that does not work
-                        --       on powershell. Need to hack the path to the underlying binary.
-                        local lsp_bin = vim.fn.stdpath("data") .. "/mason/packages/" .. server.wincmd
-                        if server.cmd == nil then
-                            server.cmd = { lsp_bin }
-                        else
-                            server.cmd[1] = lsp_bin
-                        end
-                    end
-
-                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                    server.capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities)
                     require("lspconfig")[server_name].setup(server)
                 end,
             },
